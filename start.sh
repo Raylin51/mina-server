3c9a3c31c0e3f4bc0af8c50cad7affb68cbf7bfd
linzhanjie <linzhanjie@meican.com>
2021-01-04 16:28:22 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install sudo
+apt-get install apt-transport-https
+echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
+sudo apt-get update
+sudo apt-get install -y curl unzip mina-testnet-postake-medium-curves=0.2.2-b7eff8e --allow-downgrades
+mkdir ~/keys
+wget -O ~/peers.txt https://raw.githubusercontent.com/MinaProtocol/coda-automation/bug-bounty-net/terraform/testnets/testworld/peers.txt
+touch ~/.mina-env
