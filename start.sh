731b098db6614020ee020026703c0a00754f065a
Raylin51 <raylzj51@gmail.com>
2021-01-04 16:58:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get update
 apt-get install sudo
 apt-get install apt-transport-https
 echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
