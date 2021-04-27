714bec6ea1a55877a8606eb8be52e9e63d41b484
林湛杰 <linzhanjie@meican.com>
2021-04-28 00:56:12 +0800

    fix file

--- nym.sh
+++ nym.sh
@@ -14,20 +14,20 @@ ID=$(openssl rand -base64 8)
 cd target/release
 ./nym-mixnode init --id $ID --host $(curl ifconfig.me)
 echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
-FILE="[Unit]
-Description=Nym Mixnode (0.10.0)
+FILE="[Unit] \
+Description=Nym Mixnode (0.10.0) \
+\
+[Service] \
+User=root \
+ExecStart=/root/nym/target/release/nym-mixnode run --id ${ID} \
+KillSignal=SIGINT # gracefully kill the process when stopping the service. Allows node to unregister cleanly. \
+Restart=on-failure \
+RestartSec=30 \
+StartLimitInterval=350 \
+StartLimitBurst=10 \
 
-[Service]
-User=root
-ExecStart=/root/nym/target/release/nym-mixnode run --id ${ID}
-KillSignal=SIGINT # gracefully kill the process when stopping the service. Allows node to unregister cleanly.
-Restart=on-failure
-RestartSec=30
-StartLimitInterval=350
-StartLimitBurst=10
-
-[Install]
-WantedBy=multi-user.target"
+[Install] \
+WantedBy=multi-user.target" \
 
 echo $FILE >> /etc/systemd/system/nym-mixnode.service
 
