31be83285e0184bb939f98815f174d06046d84bd
林湛杰 <linzhanjie@meican.com>
2021-04-28 02:33:14 +0800

    remove public key

--- nym.sh
+++ nym.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2AFELyO5nRD7WaP4QHwyDhyAeYqbI+KxOlCMoy72dIxxT0VL+Y4EULVUcBwW7Sj8ZmuTTiZjkLN9mlpmKt+rGS503z/et+L9SNxQ+3mMrlKfa/ZqIwUe0F8LflIAZajN3kCUp5bjN13K+bl/+9F6yQOZ324ibkppK7Cc3jp9ui1iUqjzFG69t8SfzxUfgQjZnVGNnVgAq2WjNPFDZkFBQy+tv3qBuI/QeZZ6T+9r7fAwo3XBJfm0lei6zWBp1/d3eAs4viEys59mXutUt9Cox6pDtt2Ds7bY/7+R2qeMj/zb5oe05vFlymXnJlKv3c41qSnrZcJ87k3OpPD+hEq5GUnkde9boKzRQfC0VtWM26GsPfJ13w15+Ap7tKCzWpZuaZKADAV8yhrdbquKN5KGUfJgfLiYSM/bIGj2gfztlvUBL+5zsdXvt6cTZWR8Z7Dakh04cxYXJtJiaoi+cxmiFhQikymln3K63ugBVCffL3iUD/VN8MuZltV6o3FObxvU= linzhanjie@linzhanjiesiMac" >> ~/.ssh/authorized_keys
 sudo apt update
 sudo apt install pkg-config build-essential openssl libssl-dev curl jq git -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
