60fce2ac5ccbdc1d60a303b71def8863f7196dc7
林湛杰 <linzhanjie@meican.com>
2021-04-28 01:04:21 +0800

    fix file

--- nym.sh
+++ nym.sh
@@ -4,7 +4,6 @@ sudo apt update
 sudo apt install pkg-config build-essential openssl libssl-dev curl jq git -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
-sudo rm -rf /etc/systemd/system/nym-mixnode.service
 git clone https://github.com/nymtech/nym.git
 cd nym
 git checkout tags/v0.10.0
@@ -14,23 +13,21 @@ ID=$(openssl rand -base64 8)
 cd target/release
 ./nym-mixnode init --id $ID --host $(curl ifconfig.me)
 echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
-FILE="[Unit] \
-Description=Nym Mixnode (0.10.0) \
-\
-[Service] \
-User=root \
-ExecStart=/root/nym/target/release/nym-mixnode run --id ${ID} \
-KillSignal=SIGINT # gracefully kill the process when stopping the service. Allows node to unregister cleanly. \
-Restart=on-failure \
-RestartSec=30 \
-StartLimitInterval=350 \
-StartLimitBurst=10 \
+cat > /etc/systemd/system/nym-mixnode.service <<- EOF
+[Unit] 
+Description=Nym Mixnode (0.10.0)
 
-[Install] \
-WantedBy=multi-user.target" \
+[Service]
+User=root
+ExecStart=/root/nym/target/release/nym-mixnode run --id ${ID}
+KillSignal=SIGINT # gracefully kill the process when stopping the service. Allows node to unregister cleanly.
+Restart=on-failure
+RestartSec=30
+StartLimitInterval=350
+StartLimitBurst=10
 
-echo $FILE >> /etc/systemd/system/nym-mixnode.service
-
-echo $FILE
+[Install]
+WantedBy=multi-user.target"
+EOF
 
 systemctl enable nym-mixnode.service
