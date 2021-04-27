3a2efded086447304bc98cafc8ecfb7c5dfca0ad
林湛杰 <linzhanjie@meican.com>
2021-04-27 23:33:27 +0800

    add nym

---
+++ nym.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install pkg-config build-essential openssl libssl-dev curl jq git -y
+git clone https://github.com/nymtech/nym.git
+cd nym
+git checkout tags/v0.10.0
+cargo build --release
+
+ID=openssl rand -base64 8
+cd target/release
+./nym-mixnode init --id $(ID) --host $(curl ifconfig.me)
+echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
+FILE="[Unit]
+Description=Nym Mixnode (0.10.0)
+
+[Service]
+User=root
+ExecStart=/root/nym/target/release/nym-mixnode run --id ${ID}
+KillSignal=SIGINT # gracefully kill the process when stopping the service. Allows node to unregister cleanly.
+Restart=on-failure
+RestartSec=30
+StartLimitInterval=350
+StartLimitBurst=10
+
+[Install]
+WantedBy=multi-user.target"
+
+echo FILE >> /etc/systemd/system/nym-mixnode.service
+
+systemctl enable nym-mixnode.service
