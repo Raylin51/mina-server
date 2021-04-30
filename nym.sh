44c92fea74775e9b7f2daac7dad9666174b628a4
林湛杰 <linzhanjie@meican.com>
2021-04-30 19:53:47 +0800

    fix nym init

--- nym.sh
+++ nym.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install pkg-config build-essential openssl libssl-dev curl jq git -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-git clone https://github.com/nymtech/nym.git
+# sudo apt update
+# sudo apt install pkg-config build-essential openssl libssl-dev curl jq git -y
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# source $HOME/.cargo/env
+# git clone https://github.com/nymtech/nym.git
 cd nym
-git checkout tags/v0.10.0
-cargo build --release
+# git checkout tags/v0.10.0
+# cargo build --release
 
-ID=$(openssl rand -base64 8)
 cd target/release
-./nym-mixnode init --id $ID --host $(curl ifconfig.me)
-echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
+./nym-mixnode init --id winston-smithnode --host $(curl ifconfig.me)
+# echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
 cat > /etc/systemd/system/nym-mixnode.service <<- EOF
 [Unit] 
 Description=Nym Mixnode (0.10.0)
 
 [Service]
 User=root
-ExecStart=/root/nym/target/release/nym-mixnode run --id ${ID}
+ExecStart=/root/nym/target/release/nym-mixnode run --id winston-smithnode
 KillSignal=SIGINT # gracefully kill the process when stopping the service. Allows node to unregister cleanly.
 Restart=on-failure
 RestartSec=30
@@ -31,3 +30,4 @@ WantedBy=multi-user.target
 EOF
 
 systemctl enable nym-mixnode.service
+systemctl daemon-reload
